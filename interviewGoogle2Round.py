8f56ad83234f4312d3f6fb9a0a1e943d22831e4b
hsiaofeiker <adasteve.hlf@gmail.com>
2020-05-08 14:50:06 -0700

    加上註解 1.快速檢索 2.原題目 3.解釋題目 4.思維

--- interviewGoogle2Round.py
+++ interviewGoogle2Round.py
@@ -24,26 +24,25 @@ def break_line(oneline):
         startDateStr = datetime.strptime(operStartTime.get(info[1]), "%Y-%m-%dT%H:%M")
         endDateStr = datetime.strptime(info[0], "%Y-%m-%dT%H:%M")
         difference = endDateStr- startDateStr
-        #print(difference)
+        print(info[1], difference)
         # End-Start 後會有2種格式 1.時分秒 0:03:00 跟 2.日時分秒 1461 days, 1:00:00
-        if 'days' in str(difference):
+        if 'days' in str(difference): # day + time
             datetimesplit = str(difference).split() #[0]:天數 [2]:時分秒
 
             timesplit = datetimesplit[2].split(':')
             timesplit[0], timesplit[1], timesplit[2] = int(timesplit[0]), int(timesplit[1]), int(timesplit[2])
             difference = int(datetimesplit[0])*1440+timesplit[0]*60 + timesplit[1]
-        else:
+        else: # time only
             timesplit = str(difference).split(':')
             timesplit[0], timesplit[1], timesplit[2] = int(timesplit[0]), int(timesplit[1]), int(timesplit[2])
             difference = timesplit[0]*60 + timesplit[1]
         del operStartTime[info[1]]
-
     return difference, operationCounter
 
 def convert_Time(mins):
-    days = mins // 1440
-    hours = (mins - days * 1440) // 60
-    return days, hours, mins - days * 1440 - hours * 60
+    days = mins // 1440 # 1 day 1440 mins
+    hours = (mins - days * 1440) // 60 # 1hr 60 mins
+    return days, hours, mins % 60
 
 openLog= open('log.txt')
 oneline = openLog.readline()
@@ -54,11 +53,10 @@ while oneline:
     onelinetime, onelineop = break_line(oneline)
     if onelineop == 0:
         totaltime = totaltime + onelinetime
-        pass
     else:
         totaloperation = totaloperation+onelineop
     oneline = openLog.readline()
-
+print()
 dayA, hoursA, minA = convert_Time(totaltime//totaloperation)
 dayB, hoursB, minB = convert_Time(totaltime)
 print('Average:',dayA,'days',hoursA,'hours',minA,'minutes')
